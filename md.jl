e0b22d195439b6b81cb395f15171c1e8a7e24550
cmu-samuelch <samuelch@andrew.cmu.edu>
2023-10-05 09:36:59 -0500

    yassified variable names

--- md.jl
+++ md.jl
@@ -3,7 +3,7 @@
 # everywhere.
 
 # TODO
-# randomly initialize particle velocities with zero total momentum
+# DONE - randomly initialize particle velocities with zero total momentum
 # implement continuous force/energy with cutoff of 2.5 (dimless)
 # calculates instantaneous temperature, pressure
 # applies periodic boundary conditions and the nearest-image convention
@@ -13,33 +13,33 @@ using Plots, Printf, LinearAlgebra, Random
 
 # Reads the contents of the file into a N-by-3 array of positions.
 #
-# parameter - infile_path: path to the file to read
+# parameter - 📩: path to the file to read
 # returns: N-by-3 array of positions
-function read_infile(infile_path)
-    text = read(infile_path, String)
+function read_📩(📩)
+    text = read(📩, String)
     lines = split(text, "\n")
-    output = Array{Float64}(undef, length(lines)-1, 3)
+    📨 = Array{Float64}(undef, length(lines)-1, 3)
     for i = eachindex(lines)
         if lines[i] != ""
             vals = split(lines[i])
             for j = 1:3
-                output[i, j] = parse(Float64, vals[j])
+                📨[i, j] = parse(Float64, vals[j])
             end
         end
     end
-    return output
+    return 📨
     end
 
 # Writes positions in current state to xyz format
 #
-# parameter - rs: positions to record
+# parameter - 📍: positions to record
 # parameter - i: frame number
 # returns: string of all positions
-function generate_xyz_frame(rs, i)
-    n = size(rs)[1]
+function generate_xyz_frame(📍, i)
+    n = size(📍)[1]
     text = @sprintf("%i\nFrame %i\n", n, i)
     for i = 1:n
-        text *= @sprintf("a %f %f %f\n", rs[i,1], rs[i,2], rs[i,3])
+        text *= @sprintf("a %f %f %f\n", 📍[i,1], 📍[i,2], 📍[i,3])
     end
     return text
 end
@@ -50,14 +50,14 @@ end
 # parameter - outfile_path: location to store data
 function write_data(data, outfile_path)
     (t, cols) = size(data)
-    output = ""
+    📨 = ""
     for i = 1:t
         for j = 1:cols
-            output *= @sprintf("%f,", data[i,j])
+            📨 *= @sprintf("%f,", data[i,j])
         end
-        output *= "\n"
+        📨 *= "\n"
     end
-    write(outfile_path, output)
+    write(outfile_path, 📨)
 end
 
 # initializes velocities to a certain average
@@ -66,11 +66,11 @@ end
 # parameter - μ: average velocity
 # returns: vector of velocities
 function init_velocities(N, μ)
-    vs = zeros(N, 3)
-    vs[1:end-1, :] = randn!(zeros(N-1, 3))
-    vs[end,:] = -[sum(vs[:,1]) sum(vs[:,2]) sum(vs[:,3])]
-    vs .+= μ
-    return vs
+    🚗 = zeros(N, 3)
+    🚗[1:end-1, :] = randn!(zeros(N-1, 3))
+    🚗[end,:] = -[sum(🚗[:,1]) sum(🚗[:,2]) sum(🚗[:,3])]
+    🚗 .+= μ
+    return 🚗
 end
 
 # Calculates the force vector exerted on particle 1 from LJ potential with 
@@ -93,85 +93,99 @@ end
 
 # Computes LJ forces using current positions
 #
-# parameter - rs: positions of all particles
+# parameter - 📍: positions of all particles
 # parameter - N: number of particles
 # returns: array of forces on each particle
 # returns: total LJ potential energy of system
-function LJ_forces_and_energy(rs, N)
-    Fs = zeros(size(rs))
+function LJ_forces_and_energy(📍, N)
+    🫱 = zeros(size(📍))
     U = 0
     for i = 1:N           # for each particle
         for j = i+1:N     # for each particle that i interacts with
-            F = force_between_particles(rs[i,:], rs[j,:])
-            Fs[i,:] += F
-            Fs[j,:] -= F
-            U += LJ_potential(rs[i,:], rs[j,:])
+            F = force_between_particles(📍[i,:], 📍[j,:])
+            🫱[i,:] += F
+            🫱[j,:] -= F
+            U += LJ_potential(📍[i,:], 📍[j,:])
         end
     end
-    return Fs, U
+    return 🫱, U
 end
 
-function update_v(v, force, dt)
+function update_🚗(v, force, dt)
     v += force * dt/2
     return v
 end
 
-function update_r(rs, vs, dt)
-    rs += vs*dt
-    return rs
+function update_📍(📍, 🚗, dt)
+    📍 += 🚗*dt
+    return 📍
 end
 
-function calculate_kinetic(vs)
-    return sum(vs.^2) / 2
+function calculate_kinetic(🚗)
+    return sum(🚗.^2) / 2
 end
 
-# Simulates molecules.
+# Simulates particles.
 # 
-# parameter - rs: starting positions
-# parameter - vs: starting velocities
+# parameter - 📍: starting positions
+# parameter - 🚗: starting velocities
 # parameter - N: number of particles
 # parameter - timestep: timestep.
 # parameter - duration: timesteps to simulate for.
 # parameter - outfile: location where positions get dumped
 # returns: table with columns containing timesteps, K, U, and p-components.
-function simulate(rs, vs, N, timestep, duration, outfile)
-    output = zeros(duration, 6)
+function simulate(📍, 🚗, N, timestep, duration, outfile, resolution)
+    📨 = zeros(duration, 6)
     outfile_stream = open(outfile, "a")
 
-    Fs, _ = LJ_forces_and_energy(rs, N);
+    🫱, _ = LJ_forces_and_energy(📍, N);
     for i = 1:duration
+        if i % 1000 == 0
+            println(i, "/", duration, " timesteps complete...")
+        end
         
         # VV forward one timestep
-        vs = update_v(vs, Fs, timestep)
-        rs = update_r(rs, vs, timestep)
-        Fs, U = LJ_forces_and_energy(rs, N);
-        vs = update_v(vs, Fs, timestep)
+        🚗 = update_🚗(🚗, 🫱, timestep)
+        📍 = update_📍(📍, 🚗, timestep)
+        🫱, U = LJ_forces_and_energy(📍, N);
+        🚗 = update_🚗(🚗, 🫱, timestep)
         
         # generate some data to plot later
-        t = i*timestep; K = calculate_kinetic(vs)
-        output[i,:] = [t K U sum(vs[:,1]) sum(vs[:,2]) sum(vs[:,3])]
+        t = i*timestep; K = calculate_kinetic(🚗)
+        📨[i,:] = [t K U sum(🚗[:,1]) sum(🚗[:,2]) sum(🚗[:,3])]
         
         # write current positions to outfile as one frame
-        frame = generate_xyz_frame(rs, i)
-        println(frame)
-        write(outfile_stream, frame)
+        if i % resolution == 0
+            frame = generate_xyz_frame(📍, i)
+            # println(📨[i,:])
+            write(outfile_stream, frame)
+        end
     end
 
-    return output
+    return 📨
 end
 
 
 function main()
-    rs = read_infile("pset-2/10.txt")
-    vs = zeros(size(rs))
-    N = size(rs)[1]
+    println("running MD...")
+    # PARAMETERS TO CHANGE
+    📩 = "liquid256.txt"
+    resolution = 1
 
-    outfile = "dump.xyz"
+    📍 = read_📩(📩)
+    N = size(📍)[1]
+    🚗 = init_velocities(N, [0 1 0])
+
+    outfile = "dump-pset-3.xyz"
 
     write(outfile, "")
-    data = simulate(rs, vs, N, 0.002, 1000, outfile)
+    data = simulate(📍, 🚗, N, 0.002, 1000, outfile, resolution)
 
     write_data(data, "diagnostic.csv")
+
+    p_H = plot(data[:,1], data[:,2:3])
+    p_p = plot(data[:,1], data[:,4:6])
+    plot(p_H, p_p)
 
 end
 
